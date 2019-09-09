206b464267a0dd8ce5d78037bd0bee4f4df4f39e
liebhartdevelopment <nathaniel95975@gmail.com>
2019-09-09 13:24:45 -0500

    click events

---
+++ src/click.js
@@ -0,0 +1,62 @@
+import clickJson from "./json/click.json";
+import lottie from "lottie-web";
+import { Utily as u } from "./utily";
+
+export default class Click {
+  constructor(links) {
+    this.padding = 150;
+    links.forEach(link => {
+      if (link.className !== "menuItem") {
+        link.addEventListener(
+          "click",
+          e => {
+            e.preventDefault();
+            e.target.disabled = "true";
+
+            let width = e.target.offsetWidth + this.padding;
+            let height = width / 3 - width;
+            let container = document.createElement("div");
+            container.className = "click-confetti";
+            u.$("#confetti-holder").appendChild(container);
+
+            document.body.style.setProperty("--click-width", width + "px");
+            document.body.style.setProperty("--click-height", height + "px");
+
+            document.body.style.setProperty("--click-x", e.clientX + "px");
+            document.body.style.setProperty("--click-y", e.clientY + "px");
+
+            let tmpAnim = lottie.loadAnimation({
+              wrapper: container,
+              renderer: "svg",
+              loop: false,
+              autoplay: false,
+              animationData: clickJson,
+              width: "100%",
+              height: "100%"
+            });
+            tmpAnim.setSpeed(1);
+
+            tmpAnim.addEventListener("complete", () => {
+              setTimeout(() => {
+                if (e.target.hasAttribute("target")) {
+                  window.open(e.target.href);
+
+                  if (u.isMobile()) {
+                    document.location.href = e.target.href;
+                  }
+                } else {
+                  document.location.href = e.target.href;
+                }
+                tmpAnim.destroy();
+              }, 50);
+            });
+
+            tmpAnim.play();
+            return false;
+          },
+          true
+        );
+      }
+    });
+  }
+}
